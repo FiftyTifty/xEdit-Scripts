3e1272bf77487009cef1a2e766d6a2f0045a2b7f
MajinCry <cm@cmrms.com>
2016-09-04 00:44:04 +0100

    Large number of fixes.

--- FO4Edit Scripts/AAAFyTy - Modify Armor Resistances.pas
+++ FO4Edit Scripts/AAAFyTy - Modify Armor Resistances.pas
@@ -1,20 +1,22 @@
 unit userscript;
 
 var
-		strCombatArmor, strDCGuardArmor, strDesciplesArmor, strHeavyTrapperArmor, strLeatherArmor: string; 
-		strMarineArmor, strMetalArmor, strOperatorsArmor, strPackArmor, strRaiderArmor, strRobotArmor: string;
-		strSynthArmor, strTrapperArmor: string;
+		strCombatArmor, strDCGuardArmor, strDisciplesHeavyArmor, strDisciplesHeavyMetalArmor, strDisciplesArmor, strHeavyTrapperArmor: string; 
+		strLeatherArmor, strMarineArmor, strMetalArmor, strOperatorsHeavyArmor, strOperatorsArmor, strPackArmor, strPackHeavyArmor: string;
+		strRaiderArmor, strRobotArmor, strSynthArmor, strTrapperArmor: string;
 		
-		arraystrArmors: array [0...16] of string;
+		arraystrArmors: array [0..16] of string;
 		
 		
 function Initialize: integer;
+var iCounter: integer;
 begin
 
 	//We could just type them into the array directly
 	//But this makes it much more legible.
 	
 	AddMessage('Setting up our string array!');
+	AddMessage('Length of array is '+IntToStr(Length(arraystrArmors)));
 	
 	strCombatArmor := 'Combat ';
 	arraystrArmors[0] := strCombatArmor;
@@ -22,14 +24,14 @@ begin
 	strDCGuardArmor := 'DC Guard ';
 	arraystrArmors[1] := strDCGuardArmor;
 	
-	strDesciplesArmor := 'Desciples Spiked ';
-	arraystrArmors[2] := strDesciplesHeavyArmor;
+	strDisciplesHeavyArmor := 'Disciples Spiked ';
+	arraystrArmors[2] := strDisciplesHeavyArmor;
 	
-	strDesciplesArmor := 'Desciples Metal ';
-	arraystrArmors[3] := strDesciplesHeavyMetalArmor;
+	strDisciplesHeavyMetalArmor := 'Disciples Metal ';
+	arraystrArmors[3] := strDisciplesHeavyMetalArmor;
 	
-	strDesciplesArmor := 'Desciples ';
-	arraystrArmors[4] := strDesciplesArmor;
+	strDisciplesArmor := 'Disciples ';
+	arraystrArmors[4] := strDisciplesArmor;
 	
 	strLeatherArmor := 'Leather ';
 	arraystrArmors[5] := strLeatherArmor;
@@ -40,7 +42,7 @@ begin
 	strMetalArmor := 'Metal ';
 	arraystrArmors[7] := strMetalArmor;
 	
-	strOperatorsArmor := 'Operators Heavy ';
+	strOperatorsHeavyArmor := 'Operators Heavy ';
 	arraystrArmors[8] := strOperatorsHeavyArmor;
 	
 	strOperatorsArmor := 'Operators ';
@@ -49,7 +51,7 @@ begin
 	strPackArmor := 'Pack Stuffed ';
 	arraystrArmors[10] := strPackArmor;
 	
-	strPackArmor := 'Pack ';
+	strPackHeavyArmor := 'Pack ';
 	arraystrArmors[11] := strPackHeavyArmor;
 	
 	strRaiderArmor := 'Raider ';
@@ -67,26 +69,40 @@ begin
 	strTrapperArmor := 'Trapper ';
 	arraystrArmors[16] := strTrapperArmor;
 	
+	{
+	for iCounter := 0 to Length(arraystrArmors) - 1 do begin
+		AddMessage('Print the array at the beginning! - '+ arraystrArmors[iCounter]);
+	end;
+	}
+	
 end;
 
 
 function GetTypeOfArmor(e: IInterface): string;
 var
-	strTypeOfArmor, strArmorName: string = 'Not to be processed';
+	strTypeOfArmor, strArmorName: string;
+	iCounter: integer;
 begin
-
+	
+	//AddMessage('The Name of the armor is: '+GetEditValue(ElementBySignature(e, 'FULL')));
+	
+	strTypeOfArmor := 'Not to be processed';
+	
 	strArmorName := GetEditValue(ElementByPath(e, 'FULL - Name'));
 	
-	for i := 0 to Length(arraystrArmors) - 1 do begin
+	for iCounter := 0 to Length(arraystrArmors) - 1 do begin
 	
-		if pos(arraystrArmors[i], strArmorName) = 1 then //if the beginning of strArmorName begins with one of the strings we defined in Initialize
-			strTypeOfArmor := arraystrArmors[i]; //We know what type of armour we're looking at (leather, raider, heavy trapper, etc.)
+		//AddMessage(arraystrArmors[iCounter]);
+	
+		if pos(arraystrArmors[iCounter], strArmorName) = 1 then //if the beginning of strArmorName begins with one of the strings we defined in Initialize
+			strTypeOfArmor := arraystrArmors[iCounter]; //We know what type of armour we're looking at (leather, raider, heavy trapper, etc.)
 			
-		if strTypeOfArmor = arraystrArmors[i] then //Since we know what we're working with
+		if strTypeOfArmor = arraystrArmors[iCounter] then //Since we know what we're working with
 			break; //Exit the "for" loop.
 			
 	end;
 	
+	//AddMessage('Got the armor: '+ strTypeOfArmor);
 	Result := strTypeOfArmor; //Returns the type of armour we're dealing with; 'Synth ', 'Robot ', 'Raider ', etc.
 	
 end;
@@ -95,44 +111,90 @@ end;
 function GetLimbOrChestPiece(e: IInterface): string;
 var
 	strArmorName: string;
-	strResult: string = 'Neither';
+	strResult: string;
 begin
+	
+	//AddMessage('Getting limb or chest piece');
+	
+	strResult := 'Neither';
 	
 	strArmorName := GetEditValue(ElementByPath(e, 'FULL - Name'));
 	
 	if Pos('Left Leg', strArmorName) > 0 then
 		strResult := 'Limb';
 	
-	if Pos('Right Leg', strArmorName) > 0 then
-		strResult := 'Limb';
-	
 	if Pos('Left Arm', strArmorName) > 0 then
 		strResult := 'Limb';
 	
+	if Pos('Left Bracer', strArmorName) > 0 then
+		strResult := 'Limb';
+		
+	if Pos('Left Forearm', strArmorName) > 0 then
+		strResult := 'Limb';
+	
+	if Pos('Left Shoulder', strArmorName) > 0 then
+		strResult := 'Limb';
+	
+	if Pos('Right Leg', strArmorName) > 0 then
+		strResult := 'Limb';
+		
 	if Pos('Right Arm', strArmorName) > 0 then
+		strResult := 'Limb';
+	
+	if Pos('Right Bracer', strArmorName) > 0 then
+		strResult := 'Limb';
+	
+	if Pos('Right Forearm', strArmorName) > 0 then
+		strResult := 'Limb';
+	
+	if Pos('Right Shoulder', strArmorName) > 0 then
 		strResult := 'Limb';
 	
 	if Pos(' Helmet', strArmorName) > 0 then
 		strResult := 'Limb';
 	
+	if Pos(' Mask', strArmorName) > 0 then
+		strResult := 'Limb';
+	
+	if Pos(' Helm', strArmorName) > 0 then
+		strResult := 'Limb';
+	
+	if Pos(' Head', strArmorName) > 0 then
+		strResult := 'Limb';
+	
+	
 	if Pos(' Chest Piece', strArmorName) > 0 then
 		strResult := 'Chest';
-		
+	
+	if Pos(' Chest Armor', strArmorName) > 0 then
+		strResult := 'Chest';
+	
+	if Pos(' Umpire''s Pads', strArmorName) > 0 then
+		strResult := 'Chest';
+	
+	AddMessage(strResult);
+	
 	Result := strResult;
 end;
 
 
 procedure ChangeDefenceStats(eArmorRating, eResistancesContainer: IInterface;
 															bIsChestArmor: boolean;
-															strArmorResist, strEnergyResist, strRadiationResist: string = '1');
+															strArmorResist, strEnergyResist, strRadiationResist: string);
 
 var
 	iNumberOfResistances: integer;
 	eCurrentResistance, eAddedResistance: IInterface;
 	strCurrentResistanceName: string;
-	bEnergyResistExists, bRadiationResistExists: boolean = false;
+	bEnergyResistExists, bRadiationResistExists: boolean;
+	iCounter: integer;
 		
 begin
+	
+	AddMessage('Changing stats!');
+	
+	bEnergyResistExists := false;
+	bRadiationResistExists := false;
 	
 	iNumberOfResistances := ElementCount(eResistancesContainer);
 	
@@ -146,19 +208,19 @@ begin
 	if iNumberOfResistances > 0 then
 		for iCounter := 0 to ElementCount(eResistancesContainer) - 1 do begin
 			
-			eCurrentResistance := eResistancesContainer[iCounter];
-			strCurrentResistanceName := GetEditValues(eCurrentResistance[0]);
+			eCurrentResistance := ElementByIndex(eResistancesContainer, iCounter);
+			strCurrentResistanceName := GetEditValue(ElementByIndex(eCurrentResistance, 0));
 			
 			if Pos('dtEnergy', strCurrentResistanceName) > 0 then
 				if bIsChestArmor then begin //If it's chest armor, don't halve the resist value.
-					SetEditValue(eCurrentResistance[1], strEnergyResist);
+					SetEditValue(ElementByIndex(eCurrentResistance, 1), strEnergyResist);
 					if bEnergyResistExists = false then //Since the "if Pos()" statement ran, we know that the energy resist exists.
 						bEnergyResistExists := true;
 				end;
 				
 			if Pos('dtEnergy', strCurrentResistanceName) > 0 then
 				if (bIsChestArmor = false) then begin //Since it's not chest armor, halve the resist value.
-					SetEditValue(eCurrentResistance[1], inttostr(strtoint(strEnergyResist) div 2));
+					SetEditValue(ElementByIndex(eCurrentResistance, 1), inttostr(strtoint(strEnergyResist) div 2));
 					if bEnergyResistExists = false then
 						bEnergyResistExists := true;
 				end;
@@ -166,14 +228,14 @@ begin
 			//Do the same checks for radiation resistance.
 			if Pos('dtRadiation', strCurrentResistanceName) > 0 then
 				if bIsChestArmor then begin
-					SetEditValue(eCurrentResistance[1], strRadiationResist);
+					SetEditValue(ElementByIndex(eCurrentResistance, 1), strRadiationResist);
 					if bRadiationResistExists = false then //Since the "if Pos()" statement ran, we know that the radiation resist exists.
 						bRadiationResistExists := true;
 				end;
 			
 			if Pos('dtRadiation', strCurrentResistanceName) > 0 then
 				if (bIsChestArmor = false) then begin
-					SetEditValue(eCurrentResistance[1], inttostr(strtoint(strRadiationResist) div 2));
+					SetEditValue(ElementByIndex(eCurrentResistance, 1), inttostr(strtoint(strRadiationResist) div 2));
 					if bRadiationResistExists = false then
 						bRadiationResistExists := true;
 				end;
@@ -183,12 +245,12 @@ begin
 	if bEnergyResistExists = false then begin
 		
 		eAddedResistance := ElementAssign(eResistancesContainer, HighInteger, nil, false);
-		SetEditValue(eAddedResistance[0], 'dtEnergy [DMGT:00060A81]');
+		SetEditValue(ElementByIndex(eAddedResistance, 0), 'dtEnergy [DMGT:00060A81]');
 		
 		if bIsChestArmor = false then
-			SetEditValue(eAddedResistance[1], inttostr(strtoint(strEnergyResist div 2)));
+			SetEditValue(ElementByIndex(eAddedResistance, 1), inttostr(strtoint(strEnergyResist div 2)))
 		else
-			SetEditValue(eAddedResistance[1], strEnergyResist);
+			SetEditValue(ElementByIndex(eAddedResistance, 1), strEnergyResist);
 		
 		bEnergyResistExists := true;
 	end;
@@ -196,19 +258,19 @@ begin
 	if bRadiationResistExists = false then begin
 		
 		eAddedResistance := ElementAssign(eResistancesContainer, HighInteger, nil, false);
-		SetEditValue(eAddedResistance[0], 'ddtRadiationExposure [DMGT:00060A85]');
+		SetEditValue(ElementByIndex(eAddedResistance, 0), 'ddtRadiationExposure [DMGT:00060A85]');
 		
 		if bIsChestArmor = false then
-			SetEditValue(eAddedResistance[1], inttostr(strtoint(strRadiationResist div 2)));
+			SetEditValue(ElementByIndex(eAddedResistance, 1), inttostr(strtoint(strRadiationResist div 2)))
 		else
-			SetEditValue(eAddedResistance[1], strRadiationResist);
+			SetEditValue(ElementByIndex(eAddedResistance, 1), strRadiationResist);
 		
 		bEnergyResistExists := true;
 	end;
 				
 	
 	if bIsChestArmor then
-		SetEditValue(eArmorRating, strArmorResist); //If it's a piece of chest armor, we give it an un-molested armor rating.
+		SetEditValue(eArmorRating, strArmorResist) //If it's a piece of chest armor, we give it an un-molested armor rating.
 	else
 		SetEditValue(eArmorRating, inttostr(strtoint(strArmorResist) div 2)); //It's not chest armour, so halve it.
 	
@@ -234,12 +296,13 @@ begin
 		exit;
 	
 	if GetLimbOrChestPiece(e) = 'Chest' then
-		bIsChestPiece := true;
+		bIsChestPiece := true
 	else
 		bIsChestPiece := false;
 		
 	if GetTypeOfArmor(e) = 'Not to be processed' then
 		exit;
+	
 	
 	if GetTypeOfArmor(e) = strCombatArmor then
 		ChangeDefenceStats(eArmorRating, eResistancesContainer, bIsChestPiece, '12', '6', '3');
@@ -247,13 +310,13 @@ begin
 	if GetTypeOfArmor(e) = strDCGuardArmor then
 		ChangeDefenceStats(eArmorRating, eResistancesContainer, bIsChestPiece, '12', '4', '2');
 	
-	if GetTypeOfArmor(e) = strDesciplesHeavyArmor then
+	if GetTypeOfArmor(e) = strDisciplesHeavyArmor then
 		ChangeDefenceStats(eArmorRating, eResistancesContainer, bIsChestPiece, '10', '10', '6');
 		
-	if GetTypeOfArmor(e) = strDesciplesHeavyMetalArmor then
+	if GetTypeOfArmor(e) = strDisciplesHeavyMetalArmor then
 		ChangeDefenceStats(eArmorRating, eResistancesContainer, bIsChestPiece, '12', '4', '10');
 	
-	if GetTypeOfArmor(e) = strDesciplesArmor then
+	if GetTypeOfArmor(e) = strDisciplesArmor then
 		ChangeDefenceStats(eArmorRating, eResistancesContainer, bIsChestPiece, '8', '12', '4');
 	
 	if GetTypeOfArmor(e) = strLeatherArmor then
