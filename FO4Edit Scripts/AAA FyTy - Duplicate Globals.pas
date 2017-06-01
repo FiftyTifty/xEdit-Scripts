c069f74c8f4903e92ac732a6970b298bc75d0cac
MajinCry <cm@cmrms.com>
2017-06-01 20:38:53 +0100

    Uploaded

--- FO4Edit Scripts/AAA FyTy - Duplicate Globals.pas
+++ FO4Edit Scripts/AAA FyTy - Duplicate Globals.pas
@@ -1,1 +1,69 @@
-G:/Games/Fallout 4/Edit Scripts/AAA FyTy - Duplicate Globals.pas
+unit userscript;
+var
+	tstrlistGlobalRecords: TStringList;
+	fileESP: IInterface;
+	iNumDuplicates: byte;
+	strToFind, strBaseSuffix: string;
+
+
+function Initialize: integer;
+begin
+  tstrlistGlobalRecords := TStringList.Create;
+	iNumDuplicates := 9;
+	strToFind := 'Party01';
+	strBaseSuffix := 'Party';
+end;
+
+
+function Process(e: IInterface): integer;
+var
+	strFormID: string;
+begin
+	
+	if Signature(e) <> 'GLOB' then
+		exit;
+	
+  AddMessage('Processing: ' + FullPath(e));
+	
+	fileESP := GetFile(e);
+	
+	strFormID := FormID(e);
+	tstrlistGlobalRecords.Add(strFormID);
+	
+end;
+
+
+function Finalize: integer;
+var
+	iCounter, iSuffixCounter, iRecordCounter: byte;
+	strSuffix, strEDID, strNewEDID: string;
+	eGlobal, eNewRecord: IInterface;
+begin
+  
+	iSuffixCounter := 01;
+	
+	for iCounter := 0 to tstrlistGlobalRecords.Count - 1 do begin
+		
+		inc(iSuffixCounter);
+		
+		while iRecordCounter < 9 do begin
+			eGlobal := RecordByFormID(fileESP, tstrlistGlobalRecords[iCounter], true);
+			eNewRecord := wbCopyElementToFile(eGlobal, fileESP, true, true);
+			
+			strEDID := GetElementEditValues(eNewRecord, 'EDID');
+			strSuffix := Format('%0.2d', [iSuffixCounter]);
+			
+			strNewEDID := StringReplace(strEDID, strToFind, strBaseSuffix + strSuffix);
+			
+			SetElementEditValues(eNewRecord, 'EDID', strNewEDID);
+			
+			inc(iRecordCounter);
+		end;
+		
+		iRecordCounter := 0;
+		
+	end;
+	
+end;
+
+end.
